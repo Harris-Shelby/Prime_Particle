98a819e155aadeec3171e3e02e3eab66d754af46
Harris Shelby <qsobiaolian@gmail.com>
2021-01-08 23:25:55 +0800

    Add auth function

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 // name email photo password passwordconfirm
 
 const userSchema = new mongoose.Schema({
@@ -20,12 +21,36 @@ const userSchema = new mongoose.Schema({
 		required: [true, 'Please provide a password'],
 		minlength: 8,
 		maxlength: 16,
+		select: false,
 	},
 	passwordConfirm: {
 		type: String,
 		required: [true, 'Please confirm your password'],
+		validate: {
+			validator: function (el) {
+				return el === this.password;
+			},
+			message: 'Password are not the same!',
+		},
 	},
 });
+
+userSchema.pre('save', async function (next) {
+	// only run this function if password was actually modified
+	if (!this.isModified('password')) return next();
+
+	// Hash the password with cost of 12
+	this.password = await bcrypt.hash(this.password, 12);
+
+	this.passwordConfirm = undefined;
+});
+
+userSchema.methods.correctPassword = async function (
+	candidatePassword,
+	userPassword,
+) {
+	return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
