4cfb85bed5334cc442dbe46095f2ba874fbf2546
Harris Shelby <qsobiaolian@gmail.com>
2021-01-12 12:26:53 +0800

    fix errController issue

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -58,21 +58,25 @@ const sendErrorProd = (err, res) => {
 };
 
 module.exports = (err, req, res, next) => {
+	// console.log(err.stack);
+
 	err.statusCode = err.statusCode || 500;
 	err.status = err.status || 'error';
 
 	if (process.env.NODE_ENV === 'development') {
-		sendErrorDev(err, res);
+		sendErrorDev(err, req, res);
 	} else if (process.env.NODE_ENV === 'production') {
-		console.log(err.name);
 		let error = { ...err };
-		if (err.name === 'CastError') error = handleCastErrorDB(error);
-		if (err.code === 11000) error = handleDuplicateFieldsDB(error);
-		if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
-		if (err.name === 'JsonWebTokenError') error = handleJWTError(error);
-		if (err.name === 'TokenExpiredError') error = handleJWTExpiredError(error);
+		error.message = err.message;
+
+		if (error.name === 'CastError') error = handleCastErrorDB(error);
+		if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+		if (error.name === 'ValidationError')
+			error = handleValidationErrorDB(error);
+		if (error.name === 'JsonWebTokenError') error = handleJWTError();
+		if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
+
 		sendErrorProd(error, res);
 	}
-
 	next();
 };
