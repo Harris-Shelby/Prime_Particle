5e94c2142a04aa82666f91f142732390dec22802
Harris Shelby <qsobiaolian@gmail.com>
2021-01-12 12:32:03 +0800

    fix errController dev issue

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -64,7 +64,7 @@ module.exports = (err, req, res, next) => {
 	err.status = err.status || 'error';
 
 	if (process.env.NODE_ENV === 'development') {
-		sendErrorDev(err, req, res);
+		sendErrorDev(err, res);
 	} else if (process.env.NODE_ENV === 'production') {
 		let error = { ...err };
 		error.message = err.message;
