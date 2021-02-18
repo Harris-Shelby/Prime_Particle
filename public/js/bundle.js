6f9452f8db360a1cdb2787c7d0b248204d72fcab
Harris Shelby <qsobiaolian@gmail.com>
2021-02-18 22:33:47 +0800

    fix log API URL

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8501,7 +8501,7 @@ var login = /*#__PURE__*/function () {
             _context.next = 4;
             return (0, _axios.default)({
               method: 'POST',
-              url: 'http://penguin.linux.test:4000/api/v1/users/login',
+              url: 'https://sakura.qsomula.top/api/v1/users/login',
               data: {
                 email: email,
                 password: password
@@ -8860,7 +8860,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41975" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45295" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
