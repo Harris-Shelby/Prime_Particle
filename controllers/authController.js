4c62b4141a2e2b9772e26b79cdbfb8abd7133dba
Harris Shelby <qsobiaolian@gmail.com>
2021-04-07 21:29:49 +0800

    Add passwordReset function

--- controllers/authController.js
+++ controllers/authController.js
@@ -156,17 +156,14 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 	const resetToken = user.createPasswordResetToken();
 	await user.save({ validateBeforeSave: false });
 	// send it to user's email
-	const resetUrl = `${req.protocol}://${req.get(
-		'host',
-	)}/api/v1/users/resetPassword/${resetToken}`;
 
-	const message = `Forget your password? Submit a Patch request with your new password and passwordConfirm to ${resetUrl}.\nIf you didn't forget your password please ignore this email!`;
+	// const message = `Forget your password? Submit a Patch request with your new password and passwordConfirm to ${resetUrl}.\nIf you didn't forget your password please ignore this email!`;
 	try {
-		// await sendEmail({
-		// 	email: user.email,
-		// 	subject: 'Your password reset token only valid for 10min',
-		// 	message,
-		// });
+		const resetUrl = `${req.protocol}://${req.get(
+			'host',
+		)}/api/v1/users/resetPassword/${resetToken}`;
+
+		await new Email(user, resetUrl).sendPasswordReset();
 
 		res.status(200).json({
 			status: 'success',
--- utils/email.js
+++ utils/email.js
@@ -50,4 +50,11 @@ module.exports = class Email {
 	async sendWelcome() {
 		await this.send('welcome', 'Welcome to the Shelby family!');
 	}
+
+	async sendPasswordReset() {
+		await this.send(
+			'passwordReset',
+			'Your password reset token (only valid for 10min)',
+		);
+	}
 };
