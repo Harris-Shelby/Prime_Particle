1e289c9fb5c38f12c8a05d31b468ef1de360a37c
Harris_Shelby <qsobiaolian@gmail.com>
2021-05-04 22:06:46 +0800

    test

---
+++ public/js/calendar.js
@@ -0,0 +1,75 @@
+import moment from 'moment';
+
+export const CALENDAR = class CALENDAR {
+	constructor(day) {
+		this.currDay = moment(day).format('E') * 1;
+		this.currWeek = [];
+		this.getCurrWeek(day);
+	}
+	async getPreWeek() {
+		const newcurrWeek = await this.currWeek.map((day) => {
+			day.newDay = day.newDay.subtract(7, 'days');
+			day.dayofWeek = day.newDay.format('ddd');
+			day.monofYear = day.newDay.format('MMM');
+			day.dayofMonth = day.newDay.format('DD');
+		});
+		this.currWeek = newcurrWeek;
+	}
+	async getNextWeek() {
+		const newcurrWeek = await this.currWeek.map((day) => {
+			day.newDay = day.newDay.add(7, 'days');
+			day.dayofWeek = day.newDay.format('ddd');
+			day.monofYear = day.newDay.format('MMM');
+			day.dayofMonth = day.newDay.format('DD');
+		});
+		this.currWeek = newcurrWeek;
+	}
+	getCurrWeek(dayy) {
+		[...Array(this.currDay)].forEach((day, index) => {
+			const today = false;
+			const id = this.currDay - index;
+			const newDay = moment(dayy).subtract(id, 'days');
+			const dayofWeek = newDay.format('ddd');
+			const monofYear = newDay.format('MMM');
+			const dayofMonth = newDay.format('DD');
+			this.currWeek.push({
+				today,
+				dayofWeek,
+				monofYear,
+				dayofMonth,
+				newDay,
+			});
+		});
+		if (this.currDay === 7) {
+			const today = true;
+			const newDay = moment(dayy);
+			const dayofWeek = moment(dayy).format('ddd');
+			const monofYear = moment(dayy).format('MMM');
+			const dayofMonth = moment(dayy).format('DD');
+			this.currWeek.push({
+				today,
+				dayofWeek,
+				monofYear,
+				dayofMonth,
+				newDay,
+			});
+			this.currWeek.splice(0, 1);
+		} else {
+			[...Array(7 - this.currDay)].forEach((day, index) => {
+				let today;
+				index === 0 ? (today = true) : (today = false);
+				const newDay = moment(dayy).add(index, 'days');
+				const dayofWeek = newDay.format('ddd');
+				const monofYear = newDay.format('MMM');
+				const dayofMonth = newDay.format('DD');
+				this.currWeek.push({
+					today,
+					dayofWeek,
+					monofYear,
+					dayofMonth,
+					newDay,
+				});
+			});
+		}
+	}
+};
--- public/js/index.js
+++ public/js/index.js
@@ -1,127 +1,168 @@
 /*eslint-disable*/
-import '@babel/polyfill'
-import { displayMap } from './mapbox'
-import { getAccesser } from './getFun'
-import { login, logout} from './login'
-import { updateSetting } from './updateSettings'
+import '@babel/polyfill';
+import { displayMap } from './mapbox';
+import { getAccesser } from './getFun';
+import { login, logout } from './login';
+import { updateSetting } from './updateSettings';
+import { CALENDAR } from './calendar';
 // DOM elememt
 const signUpButton = document.getElementById('signup');
 const signInButton = document.getElementById('signin');
 const container = document.getElementById('formContainer');
 const loginForm = document.getElementById('loginForm');
-const logOutBtn = document.getElementById('logout')
-const userDataForm = document.querySelector('.form-user-data')
+const logOutBtn = document.getElementById('logout');
+const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
 const timelinebox = document.querySelector('.section-timelinebox');
 const preWeek = document.getElementById('preWeek');
 const nextWeek = document.getElementById('nextWeek');
-const devicesList = document.querySelector('.devices-list')
+const devicesList = document.querySelector('.devices-list');
 
-const mapBox = document.getElementById('map')
+const mapBox = document.getElementById('map');
 
-const html = '<li class="devices__item"><span>{{deviceInfo}}</span><span>{{city}}</span></li>'
+const html =
+	'<li class="devices__item"><span>{{deviceInfo}}</span><span>{{city}}</span></li>';
+
+const timeLineboxHtml =
+	'<a class="section-timelinebox__item {{DayClass}}" href="#" id={{newDay}}><div class="day">{{dayofWeek}}</div><div class="day-number">{{dayofMonth}}</div><div class="day-dot"></div></a>';
 // Values
-
+let timeLineboxData = new CALENDAR();
 
 if (mapBox) {
-    const locations = JSON.parse(mapBox.dataset.locations);
-    displayMap(locations)
+	const locations = JSON.parse(mapBox.dataset.locations);
+	displayMap(locations);
 }
 if (signInButton || signUpButton) {
-    signUpButton.addEventListener('click', () => {
-        container.classList.add('right-panel-active');
-    });
-    signInButton.addEventListener('click', () => {
-        container.classList.remove('right-panel-active');
-    });
+	signUpButton.addEventListener('click', () => {
+		container.classList.add('right-panel-active');
+	});
+	signInButton.addEventListener('click', () => {
+		container.classList.remove('right-panel-active');
+	});
 }
 
-if(logOutBtn) {
-    logOutBtn.addEventListener('click', logout)
+if (logOutBtn) {
+	logOutBtn.addEventListener('click', logout);
 }
 
-
-if(loginForm) {
-    loginForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const email = document.getElementById('loginEmail').value;
-        const password = document.getElementById('loginPasswd').value;
-        login(email, password);
-    });
+if (loginForm) {
+	loginForm.addEventListener('submit', (e) => {
+		e.preventDefault();
+		const email = document.getElementById('loginEmail').value;
+		const password = document.getElementById('loginPasswd').value;
+		login(email, password);
+	});
 }
 
-if(userDataForm) {
-    userDataForm.addEventListener('submit', e => {
-        e.preventDefault();
-        const form = new FormData();
-        form.append('name', document.getElementById('name').value);
-        form.append('email', document.getElementById('email').value)
-        form.append('photo', document.getElementById('photo').files[0]);
-        console.log(form);
-        
-        updateSetting(form, 'data');
-    })
+if (userDataForm) {
+	userDataForm.addEventListener('submit', (e) => {
+		e.preventDefault();
+		const form = new FormData();
+		form.append('name', document.getElementById('name').value);
+		form.append('email', document.getElementById('email').value);
+		form.append('photo', document.getElementById('photo').files[0]);
+		console.log(form);
+
+		updateSetting(form, 'data');
+	});
 }
 
-if(userPasswordForm) {
-    userPasswordForm.addEventListener('submit', async function (e) {
-            e.preventDefault();
-            document.querySelector('.btn--save--password').textContent = 'Updating...';
-            const passwordCurrent = document.getElementById('password-current').value;
-            const password = document.getElementById('password').value;
-            const passwordConfirm = document.getElementById('password-confirm').value;
-            await updateSetting({ passwordCurrent, password, passwordConfirm }, 'password');
+if (userPasswordForm) {
+	userPasswordForm.addEventListener('submit', async function (e) {
+		e.preventDefault();
+		document.querySelector('.btn--save--password').textContent = 'Updating...';
+		const passwordCurrent = document.getElementById('password-current').value;
+		const password = document.getElementById('password').value;
+		const passwordConfirm = document.getElementById('password-confirm').value;
+		await updateSetting(
+			{ passwordCurrent, password, passwordConfirm },
+			'password',
+		);
 
-            document.querySelector('.btn--save--password').textContent = 'Save password';
-            document.getElementById('password-current').value = '';
-            document.getElementById('password').value = '';
-            document.getElementById('password-confirm').value = '';
-        })
+		document.querySelector('.btn--save--password').textContent =
+			'Save password';
+		document.getElementById('password-current').value = '';
+		document.getElementById('password').value = '';
+		document.getElementById('password-confirm').value = '';
+	});
 }
 
 if (timelinebox) {
-    timelinebox.addEventListener('click', async e => {
-        let newHTML = ''
-        e.preventDefault();
-        const isTimelineboxItem = Object.values(e.target.classList).indexOf('section-timelinebox__item')
-        const isTimelineboxItem_child = Object.values(e.target.parentNode.classList).indexOf('section-timelinebox__item')
-        if (isTimelineboxItem > -1) {
-            document.querySelector('.section-timelinebox__item.active').classList.remove('active');
-            e.target.classList.add('active')
-            const newDateAccesser = await getAccesser(e.target.id)
-            const newLocationData = newDateAccesser.map((accesser) => {
-                return accesser.relegation[0]
-            })
-            devicesList.querySelectorAll('*').forEach(n => n.remove())
-            newDateAccesser.forEach(e => {
-                let html1 = html.replace('{{deviceInfo}}', e.deviceInfo[0])
-                let html2 = html1.replace('{{city}}',e.city[0])
-                newHTML += html2
-            });
-            displayMap(newLocationData)
-            devicesList.insertAdjacentHTML('afterbegin', newHTML);
-        } else if (isTimelineboxItem_child > -1) {
-            document.querySelector('.section-timelinebox__item.active').classList.remove('active');
-            e.target.parentNode.classList.add('active')
-            const newDateAccesser = await getAccesser(e.target.parentNode.id)
-            const newLocationData = newDateAccesser.map((accesser) => {
-                return accesser.relegation[0]
-            })
-            displayMap(newLocationData)
-            devicesList.querySelectorAll('*').forEach(n => n.remove())
-            newDateAccesser.forEach(e => {
-                let html1 = html.replace('{{deviceInfo}}', e.deviceInfo[0])
-                let html2 = html1.replace('{{city}}',e.city[0])
-                newHTML += html2
-            });
-            devicesList.insertAdjacentHTML('afterbegin', newHTML);
-        }
-
-    });
+	let newDayHtml = '';
+	let html1 = '';
+	timeLineboxData.currWeek.forEach((e) => {
+		if (e.today) {
+			html1 = timeLineboxHtml.replace('{{DayClass}}', 'active');
+		} else {
+			switch (e.dayofWeek) {
+				case 'Wed':
+					html1 = timeLineboxHtml.replace('{{DayClass}}', 'danger');
+					break;
+				case 'Mon':
+					html1 = timeLineboxHtml.replace('{{DayClass}}', 'warning');
+					break;
+				case 'Thu':
+					html1 = timeLineboxHtml.replace('{{DayClass}}', 'warning');
+					break;
+				default:
+					html1 = timeLineboxHtml.replace('{{DayClass}}', 'primary');
+			}
+		}
+		let html2 = html1.replace('{{newDay}}', e.newDay);
+		let html3 = html2.replace('{{dayofMonth}}', e.dayofMonth);
+		let html4 = html3.replace('{{dayofWeek}}', e.dayofWeek);
+		newDayHtml += html4;
+	});
+	timelinebox.insertAdjacentHTML('afterbegin', newDayHtml);
+	timelinebox.addEventListener('click', async (e) => {
+		let newHTML = '';
+		e.preventDefault();
+		const isTimelineboxItem = Object.values(e.target.classList).indexOf(
+			'section-timelinebox__item',
+		);
+		const isTimelineboxItem_child = Object.values(
+			e.target.parentNode.classList,
+		).indexOf('section-timelinebox__item');
+		if (isTimelineboxItem > -1) {
+			document
+				.querySelector('.section-timelinebox__item.active')
+				.classList.remove('active');
+			e.target.classList.add('active');
+			const newDateAccesser = await getAccesser(e.target.id);
+			const newLocationData = newDateAccesser.map((accesser) => {
+				return accesser.relegation[0];
+			});
+			devicesList.querySelectorAll('*').forEach((n) => n.remove());
+			newDateAccesser.forEach((e) => {
+				let html1 = html.replace('{{deviceInfo}}', e.deviceInfo[0]);
+				let html2 = html1.replace('{{city}}', e.city[0]);
+				newHTML += html2;
+			});
+			displayMap(newLocationData);
+			devicesList.insertAdjacentHTML('afterbegin', newHTML);
+		} else if (isTimelineboxItem_child > -1) {
+			document
+				.querySelector('.section-timelinebox__item.active')
+				.classList.remove('active');
+			e.target.parentNode.classList.add('active');
+			const newDateAccesser = await getAccesser(e.target.parentNode.id);
+			const newLocationData = newDateAccesser.map((accesser) => {
+				return accesser.relegation[0];
+			});
+			displayMap(newLocationData);
+			devicesList.querySelectorAll('*').forEach((n) => n.remove());
+			newDateAccesser.forEach((e) => {
+				let html1 = html.replace('{{deviceInfo}}', e.deviceInfo[0]);
+				let html2 = html1.replace('{{city}}', e.city[0]);
+				newHTML += html2;
+			});
+			devicesList.insertAdjacentHTML('afterbegin', newHTML);
+		}
+	});
 }
 
 if (preWeek) {
-    preWeek.addEventListener('click', e => {
-        console.log(e.target)
-    })
+	preWeek.addEventListener('click', (e) => {
+		console.log(e.target);
+	});
 }
