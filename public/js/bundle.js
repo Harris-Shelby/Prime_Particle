f615c58aa9d987a9b3604045931a11632fc429bf
Harris_Shelby <qsobiaolian@gmail.com>
2021-05-05 11:25:28 +0800

    fix unix ts diffent err

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -15143,7 +15143,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39489" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46373" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
