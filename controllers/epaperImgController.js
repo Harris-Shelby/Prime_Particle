c57e1b8387d6225fce94109256a728ded884b037
Harris shelby <qsobiaolian@gmail.com>
2023-02-24 21:25:01 +0800

    optimize code again

--- controllers/epaperImgController.js
+++ controllers/epaperImgController.js
@@ -3,10 +3,10 @@ const multer = require('multer');
  // const User = require('../models/userModel');
  const AppError = require('../utils/appError');
  const catchAsync = require('../utils/catchAsync');
- const fs = require('fs');
+//  const fs = require('fs');
  const floydSteinberg = require('floyd-steinberg');
  const PNG = require('pngjs').PNG;
- const getPixels = require('get-pixels');
+ const getPixelsAsync = require('get-pixels');
  let cacheData = '';
  const multerStorage = multer.memoryStorage();
 
@@ -29,99 +29,81 @@ const multer = require('multer');
  exports.uploadEpaperImg = upload.single('epaper-photo');
 
  exports.formatEpaperImg = async (req, res, next) => {
-	try {
-		if (!req.file) return next();
+		try {
+			if (!req.file) {
+				return next();
+			}
 
-		req.file.filename = `img-${Date.now()}.jpeg`;
+			const buffer = await sharp(req.file.buffer)
+				.resize({
+					width: 128,
+					height: 296,
+				})
+				.toFormat('png')
+				.png({ quality: 90 })
+				.toBuffer();
 
-		const buffer = await sharp(req.file.buffer)
-			.resize({
-				width: 128,
-				height: 296,
-			})
-			.toFormat('jpeg')
-			.png({ quality: 90 })
-			.toBuffer();
+			const pngImgData = PNG.sync.read(buffer);
+			const ditherImage = floydSteinberg(pngImgData);
+			const pixelImgData = ditherImage.data;
+			const RGBAImgData = formatRGBAs(pixelImgData);
+			const b = [];
+			let c = '';
 
-		const pixelImgData = await getPixelsPro(buffer);
-		const RGBAImgData = await formatRGBAs(pixelImgData);
+			for (let i = 0; i < RGBAImgData.length; i++) {
+				const e = RGBAImgData[i];
 
-		const binaryData = RGBAImgData.map(([r, g, b, a]) => {
-			return `${r}${g}${b}${a}` === '1111' ? '1' : '0';
-		}).join('');
+				if (`${e.join('')}` === '1111') {
+					b.push(1);
+				} else {
+					b.push(0);
+				}
+			}
 
-		const hexData = binaryData
-			.match(/.{1,8}/g)
-			.map(group => setHex(group.substr(0, 4)) + setHex(group.substr(4, 4)))
-			.join('');
+			const BinImgData = group(b, 8);
 
-		cacheData = hexData;
-	} catch (error) {
-		console.log(error);
-	}
+			for (let i = 0; i < BinImgData.length; i++) {
+				const e = BinImgData[i];
+				const bre = `${e[0]}${e[1]}${e[2]}${e[3]}`;
+				const pre = `${e[4]}${e[5]}${e[6]}${e[7]}`;
+				const newBre = setHex(bre);
+				const newPre = setHex(pre);
+				c += `${newBre}${newPre}`;
+			}
 
-	next();
-};
+			cacheData = c;
+		} catch (error) {
+			console.log(error);
+		}
 
-const getPixelsPro = async (buffer) => {
-	try {
-		const pixels = await getPixelsAsync(buffer);
-		return pixels.data.slice();
-	} catch (error) {
-		throw new Error('Bad image format');
-	}
-};
+		next();
+ };
 
-const formatRGBAs = (imagePixel) => {
-	if (!imagePixel) throw new Error('Bad image pixels');
-	return group(imagePixel.map(pixel => pixel === 255 ? 1 : 0), 4);
-};
+ const getPixelsPro = async (formatImageUrl) => {
+		try {
+			const pixels = await getPixelsAsync(formatImageUrl);
+			return pixels.data.slice();
+		} catch (error) {
+			throw new Error('Bad image path');
+		}
+ };
 
-const group = (arr, num) => {
-	num = num * 1 || 1;
-	return arr.reduce((acc, item, i) => {
-		if (i % num === 0) acc.push([]);
-		acc[acc.length - 1].push(item);
-		return acc;
-	}, []);
-};
+ const formatRGBAs = (imagePixel) => {
+		if (!imagePixel) throw new Error('Bad image pixels');
+		const a = imagePixel.map((e) => (e === 255 ? 1 : 0));
+		return group(a, 4);
+ };
 
-const setHex = (key) => {
-	switch (key) {
-		case '0001':
-			return '1';
-		case '0010':
-			return '2';
-		case '0011':
-			return '3';
-		case '0100':
-			return '4';
-		case '0101':
-			return '5';
-		case '0110':
-			return '6';
-		case '0111':
-			return '7';
-		case '1000':
-			return '8';
-		case '1001':
-			return '9';
-		case '1010':
-			return 'A';
-		case '1011':
-			return 'B';
-		case '1100':
-			return 'C';
-		case '1101':
-			return 'D';
-		case '1110':
-			return 'E';
-		case '1111':
-			return 'F';
-		default:
-			return '0';
-	}
-};
+ const group = (arr, num) => {
+		num = num * 1 || 1;
+		const ret = [];
+
+		for (let i = 0; i < arr.length; i += num) {
+			ret.push(arr.slice(i, i + num));
+		}
+
+		return ret;
+ };
 
 // const filterObj = (obj, ...allowFields) => {
 // 	const newObj = {};
